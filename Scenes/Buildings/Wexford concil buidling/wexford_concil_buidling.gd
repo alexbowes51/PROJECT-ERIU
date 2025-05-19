ac5d24756c15797c76b6e62c353f4bc3396953a8
alexbowes51 <alexbowes12@outlook.ie>
2025-05-19 01:35:13 +0100

    added concil building + enter/ exit

---
+++ Scenes/Buildings/Wexford concil buidling/wexford_concil_buidling.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@onready var control: Control = $Control
+var entered = false
+
+func _physics_process(delta):
+	if entered && Input.is_action_just_pressed("chat"):
+		get_tree().change_scene_to_file("res://Scenes/Buildings/Wexford concil buidling/inside_buidling.tscn")
+		
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.has_method("Player"):
+		control.visible = true
+		entered = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.has_method("Player"):
+		control.visible = false
+		entered = false
