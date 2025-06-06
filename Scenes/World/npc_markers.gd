f0a493227bb4ce5d00f78f8be55d9cea4c83cabd
alexbowes51 <alexbowes12@outlook.ie>
2025-06-06 17:38:19 +0100

    added NPC_Waypoints needs finishing

---
+++ Scenes/World/npc_markers.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+@onready var farmer_waypoint: Node2D = $Farmer_Waypoint
+@onready var fisher_man_waypoint: Node2D = $FisherMan_Waypoint
+@onready var polition_waypoint: Node2D = $Polition_Waypoint
+@onready var infulencer_waypoint: Node2D = $Infulencer_Waypoint
